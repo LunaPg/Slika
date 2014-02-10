a3b10611af5a2fcd7ce01e495dc36491622d2c39
lunapg <malalunapg@gmail.com>
2014-02-09 19:55:28 -0500

    Images Resize auto on page load with imagemagick and quickthumb

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express= require('express');
 var fs = require('fs');
 var app = express();
+var qt = require('quickthumb');
 
 app.use(express.static(__dirname + '/app'));
 app.use(express.static(__dirname + '/.tmp'));
-
+/** Config pour QuitchThumb :
+ * Cree une nouvelle route pour acceder au images
+ * genere par imagemagick sur /preview
+ */
+app.configure(function(){
+	app.use('/preview', qt.static(__dirname + '/app/images', {type: 'resize'}));
+});
 app.get('/', function (req, res) {
 		res.sendfile('app/index.html');
 });
