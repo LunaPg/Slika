804d76fc0f000c6b55012cafd0cb1ed800988b63
lunapg <malalunapg@gmail.com>
2014-02-17 22:20:37 -0500

    moved everything from controller to directive, add watch value on selectyear, changes the AJAX url, add isotope function

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,16 +2,38 @@
 
 angular.module('myphotosApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-		$http({
-			url: '/api/2014',
-			method: 'GET'
-			}).success(function(data, status, headers, config){
-				$scope.year = 2014;
-				$scope.images = data
+  })
+	.directive('skIsotope', function($http){
+		return {
+		  link: function ($scope, element, attrs) {
+			// wtf lol value : Informagique, 3 arguments
+			// pourrave qu'on sait pas pourquoi
+			/**
+			 * @param {string} selectYear recupere les donne du modele 
+			 * @param {string} wtg Nom du param watche
+			 * @param {undefined} lol undefined Informagique
+			 * @param {string} value value de la bariable watche
+			 */
+			  $scope.watch('selectYear', function (wtf, lol, value) {
+					console.log('year', value);
+					$http({
+					url: '/api/' + value,
+					method: 'GET'
+					}).success(function(data, status, headers, config){
+						$scope.year = 2014;
+						$scope.images = data;
+						//!\  Timeout de l'enfer Ne Pas Enlever
+						// Need pour que Isotope puisse se lancer
+						setTimeout(function () {
+							element.isotope({
+								masonryHorizontal: {
+									rowHeight: 200
+								},
+									itemSelector: '.pictures',
+							})
+						}, 100);
+					});
 				});
-  });
+			}
+		}
+	});
